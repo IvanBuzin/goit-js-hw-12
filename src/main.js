698eef1122e5ce7ee73161f20e046852c67eb142
IvanBuzin <ua.buzin@gmail.com>
2024-01-14 09:43:07 +0200

    up+

--- src/main.js
+++ src/main.js
@@ -16,73 +16,81 @@ const modal = new SimpleLightbox('.gallery a', {
 
 const per_page = 40;
 let pege = 1;
-const totalPages = Math.ceil(500 / per_page);
 let userSearch = '';
 
-form.addEventListener('submit', async event => {
+form.addEventListener('submit', toForm);
+buttonResults.addEventListener('click', toButton);
+
+async function toForm(event) {
   event.preventDefault();
-  userSearch = form.search.value.trim();
+  buttonResults.classList.add('hide')
+  gallery.innerHTML = '';
+  pege = 1;
+  userSearch = form.search.value.trin();
 
-  try {
-    loader.classList.remove('hide');
-    const images = await fetchImages();
-    renderImages(images);
-  } catch (error) {
-    console.log(error.message);
-  } finally {
-    loader.classList.add('hide');
-    console.log();
+  const images = await fetchImages();
+
+  if (images.hits.length === 0) {
+    iziToast.error({
+      position: 'topRight',
+      message: "We`re sorry, but you`ve reached the end of search results.",
+      backgroundColor: #fff,
+    });
   }
+  renderImages(images);
+}
+  
+async function toButton() {
+  buttonResults.classList.add('hide');
+  const images = await fetchImages();
+  loader.classList.add('hide');
+  buttonResults.classList.remove('hide');
 
-  buttonResults.addEventListener('click', async () => {
+  if (page >= Math.ceil(images.totalHits / per_page)) {
+    buttonResults.classList.add('hide');
+    iziToast.error({
+      position: 'topRight',
+      message: "We're sorry, but you've reached the end of search results.",
+      backgroundColor: '#fff',
+    });
+  }
+  renderImages(images);
+  moveCard();
+
+  async function fetchImages() {
+    loader.classList.remove('hide');
+    page += 1;
+
     try {
-      loader.classList.remove('hide');
-      const images = await fetchImages();
-      renderImages(images);
-
-      if (page > totalPages) {
-        iziToast.error({
-          position: 'topRight',
-          message: "We`re sorry, but you`ve reached the end of search results.",
-        });
-      }
-
-      loader.classList.add('hide');
+      const images = await axios.get('https://pixabay.com/api/', {
+        params: {
+          key: '41619692-6da96b2a0003032b895baebe3',
+          q: userSearch,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          per_page: per_page,
+          page: page,
+        },
+      });
+      return images.data;
     } catch (error) {
-      /*console.log(error.message);*/
+      console.log(error.message);
     } finally {
       loader.classList.add('hide');
-      console.log();
+      form.reset();
     }
-  });
-});
+  }
 
-async function fetchImages() {
-  page += 1;
-
-  const images = await axios.get('https://pixabay.com/api/', {
-    params: {
-      key: '41619692-6da96b2a0003032b895baebe3',
-      q: userSearch,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      per_page: per_page,
-      page: page,
-    },
-  });
-  return images.data;
-}
-
-async function renderImages(images) {
-  const markup = images.hits.reduce((
-    html, {
-      webformatURL, largeImageURL, tags, likes, views, comments, downloads
-    }
-  ) => {
-    return (
-      html +
-      `<li class='gallery-item'>
+  async function renderImages(images) {
+    const markup = images.hits.reduce((
+      html, {
+        webformatURL, largeImageURL, tags, likes, views, comments, downloads
+      }
+    ) => {
+      return (
+        html +
+        `<li class='gallery-item'>
               <a class='gallery-link' href='${largeImageURL}'>
                 <img
                     class='gallery-image'
@@ -99,13 +107,28 @@ async function renderImages(images) {
                   <li><p class='statistic'>💌 Downloads<span>${downloads}</span></p></li>
               </ul>
             </li>`
+      );
+    },
+      ''
     );
-  },
- ''
- );
 
-  gallery.insertAdjacentHTML('beforeend', markup);
-  modal.refresh();
-  buttonResults.classList.remove('hide');
+    gallery.insertAdjacentHTML('beforeend', markup);
+    modal.refresh();
+
+    if (images.hits.length === 0 || pege >= Math.ceil(images.totalHits / per_page)) {
+      buttonResults.classList.add('hide');
+    }
+    else {
+      buttonResults.classList.remove('hide');
+    }
+  }
+
+  function moveCard() {
+    const card = document.querySelector('.gallery-item');
+    const domRest = card.getBoundingClientRect().height;
+    window.scrollBy({
+      top: domRest * 2,
+      behavior: 'smoth',
+    });
+  }
 }
-
